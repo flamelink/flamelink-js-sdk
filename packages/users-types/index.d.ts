5c5ddb43ffb9c3be3106895d89bc680ceaaf3447
JP Erasmus <jperasmus11@gmail.com>
2019-03-11 15:02:16 +0200

    feat(users): Implement CF methods

--- packages/users-types/index.d.ts
+++ packages/users-types/index.d.ts
@@ -5,7 +5,6 @@ declare namespace Users {
   namespace RTDB {
     interface Get extends App.RTDB.Options {
       uid?: string
-      structure?: string
     }
 
     interface Subscribe extends Get {
@@ -30,7 +29,6 @@ declare namespace Users {
   namespace CF {
     interface Get extends App.CF.Options {
       uid?: string
-      structure?: string
     }
 
     interface Subscribe extends Get {
@@ -54,7 +52,30 @@ declare namespace Users {
   }
 
   export interface Api {
-    get(): any
+    _getPermissionsRef(permission?: string): any
+
+    ref(uid?: string): any
+
+    getRaw(options?: RTDB.Get): Promise<any>
+    getRaw(options?: CF.Get): Promise<any>
+
+    get(options?: RTDB.Get): Promise<any>
+    get(options?: CF.Get): Promise<any>
+
+    subscribeRaw(options: RTDB.Subscribe): App.UnsubscribeMethod
+    subscribeRaw(options: CF.Subscribe): App.UnsubscribeMethod
+
+    subscribe(options?: RTDB.Subscribe): App.UnsubscribeMethod
+    subscribe(options?: CF.Subscribe): App.UnsubscribeMethod
+
+    addToDB(options: RTDB.Add): Promise<any>
+    addToDB(options: CF.Add): Promise<any>
+
+    updateInDB(options: RTDB.Update): Promise<any>
+    updateInDB(options: CF.Update): Promise<any>
+
+    removeFromDB(options: RTDB.Remove): Promise<any>
+    removeFromDB(options: CF.Remove): Promise<any>
   }
 
   export type FlamelinkFactory = (context: App.Context) => Users.Api
--- packages/users/src/cf/index.ts
+++ packages/users/src/cf/index.ts
@@ -1,8 +1,196 @@
-import { FlamelinkFactory, Api } from '@flamelink/sdk-users-types'
+import chunk from 'lodash/chunk'
+import flamelink from '@flamelink/sdk-app'
+import { FlamelinkFactory, Api, CF } from '@flamelink/sdk-users-types'
+import {
+  applyOptionsForCF,
+  pluckResultFields,
+  logWarning,
+  FlamelinkError,
+  createQueue,
+  getTimestamp,
+  getCurrentUser,
+  isRefLike
+} from '@flamelink/sdk-utils'
+import { CF_BATCH_WRITE_LIMIT } from '../constants'
 
-const factory: FlamelinkFactory = function(context) {
+const USERS_COLLECTION = 'fl_users'
+const PERMISSIONS_COLLECTION = 'fl_permissions'
+
+const factory: FlamelinkFactory = context => {
   const api: Api = {
-    get: () => {}
+    ref(uid) {
+      const firestoreService = flamelink._ensureService('firestore', context)
+      const baseRef = firestoreService.collection(USERS_COLLECTION)
+      return uid ? baseRef.where('_fl_meta_.docId', '==', uid) : baseRef
+    },
+
+    getRaw({ uid, ...options }: CF.Get) {
+      return applyOptionsForCF(api.ref(uid), options).get({
+        source: options.source || 'default'
+      })
+    },
+
+    async get({ uid, ...options }: CF.Get = {}) {
+      const pluckFields = pluckResultFields(options.fields)
+
+      const snapshot = await api.getRaw({ uid, ...options })
+
+      const users: any[] = []
+      snapshot.forEach((doc: any) => users.push(doc.data()))
+
+      const plucked = pluckFields(users)
+      return uid ? plucked[0] : plucked
+    },
+
+    subscribeRaw({ uid, callback, ...options }: CF.Subscribe) {
+      const filtered = applyOptionsForCF(api.ref(uid), options)
+
+      const args = []
+
+      if (!context.usesAdminApp) {
+        args.push({
+          includeMetadataChanges: !!options.includeMetadataChanges
+        })
+      }
+
+      args.push(
+        (snapshot: any) => callback(null, snapshot),
+        (err: Error) => callback(err, null)
+      )
+
+      return filtered.onSnapshot(...args)
+    },
+
+    subscribe({ uid, callback, changeType, ...options }: CF.Subscribe) {
+      const pluckFields = pluckResultFields(options.fields)
+
+      return api.subscribeRaw({
+        uid,
+        ...options,
+        async callback(err, snapshot) {
+          if (err) {
+            return callback(err, null)
+          }
+
+          if (snapshot.empty) {
+            return callback(null, [])
+          }
+
+          const users: any[] = []
+
+          if (changeType) {
+            snapshot.docChanges().forEach((change: any) => {
+              if (change.type === changeType) {
+                users.push(change.doc.data())
+              }
+            })
+
+            if (!users.length) {
+              return
+            }
+          } else {
+            snapshot.forEach((doc: any) => users.push(doc.data()))
+          }
+
+          const plucked = pluckFields(users)
+          return callback(null, uid ? plucked[0] : plucked)
+        }
+      })
+    },
+
+    _getPermissionsRef(permissions?: string) {
+      const firestoreService = flamelink._ensureService('firestore', context)
+
+      if (!permissions) {
+        return firestoreService.doc(`${PERMISSIONS_COLLECTION}/1`)
+      }
+
+      if (isRefLike(permissions)) {
+        return permissions
+      }
+
+      return firestoreService.doc(
+        `${PERMISSIONS_COLLECTION}/${permissions
+          .replace(PERMISSIONS_COLLECTION, '')
+          .replace('/', '')}`
+      )
+    },
+
+    addToDB({ uid, data }: CF.Add) {
+      if (!uid) {
+        throw new FlamelinkError(`Please provide the user's "uid"`)
+      }
+
+      const payload =
+        typeof data === 'object'
+          ? Object.assign({}, data, {
+              _fl_meta_: {
+                createdBy: getCurrentUser(context),
+                createdDate: getTimestamp(context),
+                docId: uid
+              },
+              displayName: data.displayName || '',
+              email: data.email || '',
+              enabled: data.enabled || 'Yes',
+              firstName: data.firstName || '',
+              id: uid,
+              lastName: data.lastName || '',
+              permissions: api._getPermissionsRef(data.permissions)
+            })
+          : data
+
+      return api.ref(uid).set(payload)
+    },
+
+    async updateInDB({ uid, data }: CF.Update) {
+      if (typeof uid !== 'string' || typeof data !== 'object') {
+        throw new FlamelinkError(
+          '"updateInDB" called with the incorrect arguments. Check the docs for details.'
+        )
+      }
+
+      const payload = Object.assign({}, data, {
+        '_fl_meta_.lastModifiedBy': getCurrentUser(context),
+        '_fl_meta_.lastModifiedDate': getTimestamp(context),
+        '_fl_meta_.docId': uid,
+        id: uid,
+        permissions: api._getPermissionsRef(data.permissions)
+      })
+
+      const snapshot = await api.ref(uid).get()
+
+      if (snapshot.empty) {
+        logWarning(
+          `No user exists with a unique ID of "${uid}" - creating new user instead.`
+        )
+        return api.addToDB({ uid, data })
+      }
+
+      const users: any[] = []
+      snapshot.forEach((doc: any) => users.push(doc))
+
+      return await users[0].ref.update(payload)
+    },
+
+    async removeFromDB({ uid }: CF.Remove) {
+      const snapshot = await api.getRaw({ uid })
+
+      if (snapshot.empty) {
+        // Nothing to delete
+        return
+      }
+
+      const usersDocChunks: any[] = chunk(snapshot.docs, CF_BATCH_WRITE_LIMIT)
+      const db = flamelink._ensureService('firestore', context)
+
+      const batchQueue = createQueue(async (usersDocChunk: any[]) => {
+        const batch = db.batch()
+        usersDocChunk.forEach((usersDoc: any) => batch.delete(usersDoc))
+        return batch.commit()
+      }, usersDocChunks)
+
+      return batchQueue.start()
+    }
   }
 
   return api
