f6810f9b95019b1df3988d1e4314e7e0953a9383
JP Erasmus <jperasmus11@gmail.com>
2019-04-03 16:23:32 +0200

    chore(build): implement realtime db emulator for tests

--- scripts/run-tests-on-ci.sh
+++ scripts/run-tests-on-ci.sh
@@ -2,49 +2,107 @@
 
 # Script adapted from https://gist.github.com/danahartweg/72600e0d30ae54290bf4deb197400ee9
 
-EMULATOR="cloud-firestore-emulator"
-EMULATOR_TARGET=$(find ~/.cache/firebase/emulators/ -type f -name "$EMULATOR*.jar" | sort -r | head -n1)
-EMULATOR_PORT=8080
+############################
+# CLOUD FIRESTORE EMULATOR #
+############################
+CF_EMULATOR="cloud-firestore-emulator"
+CF_EMULATOR_TARGET=$(find ~/.cache/firebase/emulators/ -type f -name "$CF_EMULATOR*.jar" | sort -r | head -n1)
+CF_EMULATOR_PORT=8080
 
-if [ -z "$EMULATOR_TARGET" ]; then
+if [ -z "$CF_EMULATOR_TARGET" ]; then
   echo "Could not find the firestore emulator. Ending test run."
   exit 1
 fi
 
 # I've found that the java process is not always killed properly,
 # causing issues on subsequent runs... so let's clean things up when we're done (or have errored)
-killEmulatorPid()
+killFirestoreEmulatorPid()
 {
-  EMULATOR_PID=$(pgrep -f "$EMULATOR")
+  CF_EMULATOR_PID=$(pgrep -f "$CF_EMULATOR")
 
-  if ! [ -z "$EMULATOR_PID" ]; then
-    kill -9 "$EMULATOR_PID"
+  if ! [ -z "$CF_EMULATOR_PID" ]; then
+    kill -9 "$CF_EMULATOR_PID"
   fi
 }
 
-java -jar "$EMULATOR_TARGET" --host=127.0.0.1 --port=$EMULATOR_PORT > /dev/null 2> firestore-emulator.log &
-RETRIES=0
-RETRY_LIMIT=10
+java -jar "$CF_EMULATOR_TARGET" --host=127.0.0.1 --port=$CF_EMULATOR_PORT > /dev/null 2> firestore-emulator.log &
+CF_RETRIES=0
+CF_RETRY_LIMIT=10
 
-while [ $RETRIES -lt $RETRY_LIMIT ]; do
+while [ $CF_RETRIES -lt $CF_RETRY_LIMIT ]; do
   sleep 1
   echo "Pinging firestore emulator"
 
-  if nc -z localhost $EMULATOR_PORT; then
+  if nc -z localhost $CF_EMULATOR_PORT; then
     break
   fi
 
-  let RETRIES+=1
+  let CF_RETRIES+=1
 
-  if [ $RETRIES -ge $RETRY_LIMIT ]; then
+  if [ $CF_RETRIES -ge $CF_RETRY_LIMIT ]; then
     echo "Could not find the firestore emulator. Ending test run."
-    killEmulatorPid
+    killFirestoreEmulatorPid
     exit 1
   fi
 done
 
-# Now run the tests
+
+##############################
+# REALTIME DATABASE EMULATOR #
+##############################
+RTDB_EMULATOR="firebase-database-emulator"
+RTDB_EMULATOR_TARGET=$(find ~/.cache/firebase/emulators -type f -name "$RTDB_EMULATOR*.jar" | sort -r | head -n1)
+RTDB_EMULATOR_PORT=9000
+
+echo "$RTDB_EMULATOR_TARGET"
+
+if [ -z "$RTDB_EMULATOR_TARGET" ]; then
+  echo "Could not find the database emulator. Ending test run."
+  exit 1
+fi
+
+# I've found that the java process is not always killed properly,
+# causing issues on subsequent runs... so let's clean things up when we're done (or have errored)
+killDatabaseEmulatorPid()
+{
+  RTDB_EMULATOR_PID=$(pgrep -f "$RTDB_EMULATOR")
+
+  if ! [ -z "$RTDB_EMULATOR_PID" ]; then
+    kill -9 "$RTDB_EMULATOR_PID"
+  fi
+}
+
+java -jar "$RTDB_EMULATOR_TARGET" > /dev/null 2> database-emulator.log &
+RTDB_RETRIES=0
+RTDB_RETRY_LIMIT=10
+
+while [ $RTDB_RETRIES -lt $RTDB_RETRY_LIMIT ]; do
+  sleep 1
+  echo "Pinging database emulator"
+
+  if nc -z localhost $RTDB_EMULATOR_PORT; then
+    break
+  fi
+
+  let RTDB_RETRIES+=1
+
+  if [ $RTDB_RETRIES -ge $RTDB_RETRY_LIMIT ]; then
+    echo "Could not find the database emulator. Ending test run."
+    killDatabaseEmulatorPid
+    exit 1
+  fi
+done
+
+
+#####################
+# RUN ALL THE TESTS #
+#####################
 jest
 
-echo "End of test run. Cleaning up the firestore emulator."
-killEmulatorPid
+
+########################
+# CLEAN UP AFTER TESTS #
+########################
+echo "End of test run. Cleaning up the emulators."
+killFirestoreEmulatorPid
+killDatabaseEmulatorPid
