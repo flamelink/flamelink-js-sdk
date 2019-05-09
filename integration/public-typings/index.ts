049f8d950b3e481c560fa664006f847fd638cbe1
JP Erasmus <jperasmus11@gmail.com>
2019-05-09 12:33:36 +0200

    test(integration): add public typings

---
+++ integration/public-typings/index.ts
@@ -0,0 +1,7 @@
+import * as flamelink from 'flamelink/app'
+
+/**
+ * Verifying the types are properly exposed from the `flamelink` package
+ */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+let app: flamelink.App
